fef26fcd8e303c3f1671c11f8560730c104cb17a
mildbyte@gmail.com <mildbyte@gmail.com>
2011-12-27 14:21:17 +0200

    More comments on the two client forms

--- ReQuizClient/frmStart.cs
+++ ReQuizClient/frmStart.cs
@@ -14,6 +14,8 @@ namespace ReQuizClient
     public partial class frmStart : Form
     {
         private bool isConnecting = false;
+
+        //Address and port of the server to connect to
         private IPAddress serverAddr;
         private int serverPort;
 
@@ -24,9 +26,12 @@ namespace ReQuizClient
 
         private void OnConnectFailed(object sender, ConnectFailedEventArgs e)
         {
+            //Don't display the message if the connection is not relevant anymore
             if (!isConnecting) return;
+
+            //Show the error message and revert the form to the "Enter IP address" state
             MessageBox.Show("Failed to connect to the server. Check that the server IP is entered correctly and that your connection is not being blocked by a firewall." + Environment.NewLine + "(" + e.Message + ")"
-    , "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                , "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             isConnecting = false;
             btnConnect.Enabled = true;
             lblConnectionProgress.Visible = false;
@@ -38,71 +43,94 @@ namespace ReQuizClient
         {
             if (!isConnecting)
             {
+                //Validate the IP address and the port
                 try
                 {
                     serverAddr = IPAddress.Parse(txtAddress.Text.Trim());
                     serverPort = int.Parse(txtPort.Text.Trim());
                 } catch (FormatException ex) {
-                    MessageBox.Show("You have entered an invalid IP address and/or port.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("You have entered an invalid IP address and/or port.", 
+                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     txtAddress.SelectAll();
                     txtAddress.Select();
                     return;
                 }
 
+                //Hide the address fields and show the connection progress
                 isConnecting = true;
                 btnConnect.Enabled = false;
                 lblConnectionProgress.Visible = true;
                 txtAddress.Visible = false;
                 lblIPAddress.Visible = false;
 
+                //Set up the component that will receive the questions
                 QuestionReceiver questionReceiver = new QuestionReceiver();
                 questionReceiver.Server = serverAddr;
                 questionReceiver.Port = serverPort;
+
+                //Windows username is used as the username
                 questionReceiver.Username = System.Environment.UserName;
                 questionReceiver.ClientLog += LogEvent;
                 questionReceiver.QuestionsFetched += OnConnectComplete;
                 questionReceiver.ConnectFailed += OnConnectFailed;
 
+                //Start the operation
                 questionReceiver.ReceiveQuestions();
             }
         }
+
         private void LogEvent(object sender, ClientLogEventArgs e)
         {
+            //Feed the log of the server to the connection progress label
             lblConnectionProgress.Text = e.Message;
         }
+
         private void OnConnectComplete(object sender, QuestionsFetchedEventArgs e)
         {
+            //Hide the form and remove the connection progress label
             this.Hide();
             isConnecting = false;
             btnConnect.Enabled = true;
             lblConnectionProgress.Visible = false;
             txtAddress.Visible = true;
             lblIPAddress.Visible = true;
+
+            //Create the form that would display the question to the user
             frmQuestions questForm = new frmQuestions(e.Parameters);
+
+            //When the question form will be closed, request it to pass the user's answers back
             questForm.QuizCompleted += OnQuestionsClose;
             questForm.FormClosed += OnQuestionFormClose;
         }
 
         private void OnQuestionsClose(object sender, QuizCompletedEventArgs e)
         {
-            QuizAnswers qAns = e.Answers;
-            frmSubmit submitForm = new frmSubmit(qAns, serverAddr, serverPort);
+            //The user has entered the answers on the question form
+            //Create a form that would submit the answers to the server
+            frmSubmit submitForm = new frmSubmit(e.Answers, serverAddr, serverPort);
+
+            //Notify this form when the submission form is closed
             submitForm.FormClosed += OnSubmitFormClose;
         }
 
         private void OnQuestionFormClose(object sender, FormClosedEventArgs e)
         {
+            //The question form has closed
+            //Destroy the questions form and show this form
             ((frmQuestions)sender).Dispose();
             this.Show();
         }
 
         private void OnSubmitFormClose(object sender, FormClosedEventArgs e)
         {
+            //The user has viewed his results and wants to exit the application
+            //Close the main form
             this.Close();
         }
 
         private void frmStart_Load(object sender, EventArgs e)
         {
+            //Focus on the address field at startup
             txtAddress.Select();
         }
     }
--- ReQuizClient/frmSubmit.cs
+++ ReQuizClient/frmSubmit.cs
@@ -12,21 +12,35 @@ namespace ReQuizClient
 {
     public partial class frmSubmit : Form
     {
+        //User's answers to the quiz
         private QuizAnswers answers;
 
+        /// <summary>
+        /// Creates a submission form that will automatically submit the answers and
+        /// display the score to the user
+        /// </summary>
+        /// <param name="answers">User's quiz answers</param>
+        /// <param name="server">IP address of the server</param>
+        /// <param name="port">Port number on the server</param>
         public frmSubmit(QuizAnswers answers, IPAddress server, int port)
         {
+            //Set up the needed components
             InitializeComponent();
             this.answers = answers;
+
+            //Initialise the answer-sending and mark-receiving component
             MarkReceiver answerSender = new MarkReceiver();
             answerSender.Server = server;
             answerSender.Port = port;
             answerSender.Username = System.Environment.UserName;
             answerSender.Answers = answers;
 
+            //Set up the events to log connection progress and get back the 
+            //quiz result when the operation is complete
             answerSender.ClientLog += LogEvent;
             answerSender.MarkReceived += DisplayMark;
 
+            //Start the submission process
             answerSender.SendAnswersReceiveMark();
 
             Show();
@@ -34,17 +48,20 @@ namespace ReQuizClient
 
         private void LogEvent(object sender, ClientLogEventArgs e)
         {
+            //Display the text the component wants to log
             lblConnectionProgress.Text = e.Message;
         }
 
         private void DisplayMark(object sender, MarkReceivedEventArgs e)
         {
+            //Show the label with the result and the close button
             lblResult.Show();
             lblConnectionProgress.Hide();
             btnClose.Show();
 
             if (e.Result.answersAccepted)
             {
+                //If the server accepted our answers, display the result and the percentage
                 lblResult.Text = "Correct answers: " + e.Result.correctAnswers + "/" + answers.answers.Count +
                     string.Format(" ({0:#0}%)", e.Result.correctAnswers * 100 / answers.answers.Count) + Environment.NewLine +
                     "Hints used: " + answers.hintsUsed + Environment.NewLine +
@@ -52,6 +69,7 @@ namespace ReQuizClient
             }
             else
             {
+                //The user tried to take the quiz twice, report that on the label
                 lblResult.Text = "The server has reported that you have already sent the answers once." + Environment.NewLine +
                     "You cannot submit the answers twice in one session." + Environment.NewLine +
                     "Only your first result will be stored on the server.";
